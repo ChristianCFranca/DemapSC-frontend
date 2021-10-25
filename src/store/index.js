8410bab686084aa2708b5fc6fabeac10eef9662e
ChristianCFranca <62810702+ChristianCFranca@users.noreply.github.com>
2021-10-25 11:47:14 -0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -95,7 +95,7 @@ export default new Vuex.Store({
     SET_TODOS_OS_PEDIDOS(state, pedidos){
       // Ordenação por data
       if (pedidos)
-        state.pedidos = pedidos.sort((a, b) => new Date(b.dataPedido.split('/').reverse().join('-')) - new Date(a.dataPedido.split('/').reverse().join('-')));
+        state.pedidos = pedidos.sort((a, b) => a.number - b.number));
     },
     USER_CLEAR_DATA() {
       localStorage.removeItem('user');
